b26bf2b9addbb3a31312dd992f16ca7b07bbc871
John Giblin <jwgiblin3@hotmail.com>
2021-01-16 11:37:50 -0500

    aa

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static('./dist/chezjuan'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/cookiteer/'}
+  );
+  });
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
